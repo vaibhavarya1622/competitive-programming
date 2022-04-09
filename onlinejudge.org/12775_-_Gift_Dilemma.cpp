83caea9eadc6358d41e686c878a7ad5901c190f0
vaibhavarya1622 <vaibhavarya1622@gmail.com>
2022-04-09 14:38:52 +0530

    new problems solved

---
+++ onlinejudge.org/12775_-_Gift_Dilemma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll a,b,c,p;
+        cin>>a>>b>>c>>p;
+
+        for(int i=1;i<8;++i){
+            
+        }
+    }
+}
